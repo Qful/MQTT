65ead1c4bc0cd75ae47732a6680d745692304cf7
qitas <qitas@qitas.cn>
2018-09-10 17:11:38 +0800

    some fix

--- image/run.sh
+++ image/run.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
 #add you shell 
-
-service rabbitmq-server stop
-chown rabbitmq:rabbitmq /var/lib/rabbitmq/.erlang.cookie
-service rabbitmq-server start
-
 echo "in run.sh shell"
 
-rabbitmqctl delete_user  guest
-rabbitmqctl add_user  qitas  test1234
-rabbitmqctl set_user_tags qitas administrator
-rabbitmqctl list_users
+service rabbitmq-server stop
+sleep 10
+chown rabbitmq:rabbitmq /var/lib/rabbitmq/.erlang.cookie
+sleep 2
+service rabbitmq-server start
+sleep 10
+#rabbitmq-server on
 
-rabbitmq-server on
+rabbitmqctl delete_user  guest
+sleep 2
+rabbitmqctl add_user  qitas  test1234
+sleep 2
+rabbitmqctl set_user_tags qitas administrator
+sleep 2
+rabbitmqctl list_users
+sleep 2
+
+echo "done qitas configure"
