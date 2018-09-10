023bf99f8d40056ef2ec7409972733faca303352
qitas <qitas@qitas.cn>
2018-09-10 16:46:43 +0800

    test files

--- image/run.sh
+++ image/run.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
+
 #add you shell 
 
 service rabbitmq-server stop
 chown rabbitmq:rabbitmq /var/lib/rabbitmq/.erlang.cookie
 service rabbitmq-server start
 
-echo "congratulations,your rabbitmq is running."
-
-
+echo "in run.sh shell"
 
 rabbitmqctl delete_user  guest
 rabbitmqctl add_user  qitas  test1234
 rabbitmqctl set_user_tags qitas administrator
 rabbitmqctl list_users
+
+rabbitmq-server on
